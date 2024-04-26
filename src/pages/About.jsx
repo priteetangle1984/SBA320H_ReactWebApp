e7a7bac09801edbde88d76f18456d15cdb00c71f
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-26 07:51:41 -0400

    final codes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
+    <div className="page-container">
       <h2>About Card Deck API Project</h2>
-      <p>
+      <p style={{ maxWidth: "375px" }}>
         The Card Deck API project is designed to demonstrate the integration of
         the Card Deck API into a web application. The project allows users to
         perform various actions related to a deck of cards, such as shuffling
@@ -14,8 +14,7 @@ const About = () => {
       <ul>
         <li>Draw a card from the deck</li>
         <li>Shuffle the deck</li>
-        <li>Create new decks</li>
-        <li>Add cards to piles</li>
+        <li>View Deck</li>
         {/* Add more features as needed */}
       </ul>
       <h3>Technologies Used</h3>
@@ -25,16 +24,6 @@ const About = () => {
         <li>React Router</li>
         {/* Add more technologies as needed */}
       </ul>
-      <h3>Developer Information</h3>
-      <p>
-        This project was developed by [Developer Name]. For inquiries or
-        feedback, please contact [Developer Email].
-      </p>
-      <h3>Future Plans</h3>
-      <p>
-        In the future, we plan to add additional features such as card
-        animations, advanced card manipulation options, and user authentication.
-      </p>
     </div>
   );
 };
