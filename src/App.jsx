8c53ba9a00e127a3e6559511c076e6dda28a4e3f
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-26 01:00:51 -0400

    added dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
+// import Footer from "./components/Footer/Footer"; // Import the Footer component
 import Home from "./pages/Home";
-import Cars from "./pages/Contact";
+import DrawCard from "./pages/DrawCard";
+import ShuffleDeck from "./pages/ShuffleDeck";
+import ViewDeck from "./pages/ViewDeck";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,9 +15,18 @@ function App() {
       <div>
         <Nav />
         <Routes>
+          {/* Route for Home page */}
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* Route for DrawCard page */}
+          <Route path="/draw-card" element={<DrawCard />} />
+          {/* Route for ShuffleDeck page */}
+          <Route path="/shuffle-deck" element={<ShuffleDeck />} />
+          {/* Route for ViewDeck page */}
+          <Route path="/view-deck/:deckId" element={<ViewDeck />} />
+          <Route path="/about" element={<About />} />{" "}
+          {/* Route for About page */}
+          <Route path="/contact" element={<Contact />} />{" "}
+          {/* Route for Contact page */}
         </Routes>
       </div>
     </Router>
